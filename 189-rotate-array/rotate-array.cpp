0e5eb7eb07a10a4522a4b48591602c18c40198d4
Vasudev Siddh <143492267+Vasudev-Siddh@users.noreply.github.com>
2025-06-24 23:29:39 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (36.99%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        // so will you do it here or locally?
-        // here but first ill do a pseudo code on copy
+       
         // so what i came up with was like a 2 pointer thingy wher what i do is , each time i will do is , for n times  a loop will work and what it does is , we have one pointer fixed at end and we have another which starts from 0 index and traverses till second last swapping elements
         // lemme just quick code it
         // int s,f;
@@ -43,7 +42,6 @@ public:
             j--;
         }
        
-
     }  
    
    
