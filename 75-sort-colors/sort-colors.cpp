11d1f84df92ed69878d7e1b5e264e0eb4121167d
Vasudev Siddh <143492267+Vasudev-Siddh@users.noreply.github.com>
2025-06-26 10:13:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.85%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -14,15 +14,29 @@ public:
         //         break;
         //     }
         // }
-        for(int i=0;i<nums.size()-1;i++){
-            int me=i;
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[me]>nums[j])
-                    me=j;
-            }
-            if(me!=i)
-                swap(nums[me],nums[i]);
-        }
         // return nums;
+        // for(int i=0;i<nums.size()-1;i++){
+        //     int me=i;
+        //     for(int j=i+1;j<nums.size();j++){
+        //         if(nums[me]>nums[j])
+        //             me=j;
+        //     }
+        //     if(me!=i)
+        //         swap(nums[me],nums[i]);
+        // }
+        // Dutch flag ALgo
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0)
+                swap(nums[mid++],nums[low++]);
+            else if(nums[mid]==1)
+                mid++;
+            else
+                swap(nums[mid],nums[high--]);
+        }
+
+
     }
 };
