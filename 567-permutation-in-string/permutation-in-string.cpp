bcb783ea3947d199a861366cd596bd822aada0c8
Vasudev Siddh <143492267+Vasudev-Siddh@users.noreply.github.com>
2025-06-27 14:49:49 +0530

    Time: 489 ms (9.83%) | Memory: 424.8 MB (5.04%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -3,15 +3,26 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n=s1.size();
         // int s;
-        sort(s1.begin(),s1.end());
+      
         string s="";
         for(int i=0;i<s2.length();i++){
             s=s2.substr(i,s1.length());
             
-        sort(s.begin(),s.end());
-            if( s1==s)
+        
+            if(comp(s1,s))
                 return 1;
         }
         return 0;
+    }
+    bool comp(string s1, string s){
+        int fm[26] = {0};
+        for(char i : s1 ) fm[i-'a']++;
+        for(char i : s ) fm[i-'a']--;
+
+        for(int i : fm ){
+            if(i>0)
+                return 0;
+        }
+        return 1;
     }
 };
