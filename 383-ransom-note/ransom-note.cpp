c9ec99e166303daac6b9a4afd955aad241c0337d
Vasudev Siddh <143492267+Vasudev-Siddh@users.noreply.github.com>
2025-06-27 11:45:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (75.51%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     bool canConstruct(string r, string m) {
-       unordered_map<char,int> fm;
-       for(char i : r){
-            fm[i]++;
-       }
-       for(char i : m){
-            fm[i]--;
-       }
-       for(const auto& i : fm){
-            if(i.second>0)
-                return 0;
-       }
-       return 1;
+    //    unordered_map<char,int> fm;
+    //    for(char i : r){
+    //         fm[i]++;
+    //    }
+    //    for(char i : m){
+    //         fm[i]--;
+    //    }
+    //    for(const auto& i : fm){
+    //         if(i.second>0)
+    //             return 0;
+    //    }
+    //    return 1;
+
+    int fa[26]={0};
+    for(char i : r){
+        fa[i-'a']++;
+    }
+    for(char i:m){
+        if(fa[i-'a']>0)
+            fa[i-'a']--;
+    }
+    for(int i=0;i<26;i++){
+        if(fa[i]>0)
+            return 0;
+    }
+    return 1;
 
 
     }
