e8088aa0a8600fa9d52dd4e25f22d4762a9fbbca
Vasudev Siddh <143492267+Vasudev-Siddh@users.noreply.github.com>
2025-06-27 09:35:58 +0530

    Time: 7 ms (7.10%) | Memory: 10.2 MB (36.25%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string a="";
+        for(int i=s.length()-1;i>=0;i--){
+            if(isalnum(s[i])){
+                s[i]=tolower(s[i]);
+                a+=s[i];
+            }
+        }
+        
+        // b=a;
+        // reverse(b.begin(),b.end());
+        // if(b==a)
+        //     return 1;
+        // return 0;
+        int l=0,r=a.length()-1;
+        while(l<r){
+            if(a[l]!=a[r])
+                return 0;
+            l++;
+            r--;
+        }
+        return 1;
+    }
+};
