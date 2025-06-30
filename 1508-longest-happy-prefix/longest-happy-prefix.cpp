e1142416a9434dade5c0c5f78a5e5022abdf3b32
Vasudev Siddh <143492267+Vasudev-Siddh@users.noreply.github.com>
2025-06-30 11:50:08 +0530

    Time: 1 ms (97.05%) | Memory: 23.3 MB (81.36%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.cpp
+++ 1508-longest-happy-prefix/longest-happy-prefix.cpp
@@ -44,13 +44,16 @@ public:
     while(i<n){
         if(s[i]==s[len]){
             len++;
-            lps[i++]=len; 
+            lps[i]=len;
+            i++; 
         }
         else{
             if(len!=0)
                 len=lps[len-1];
-            else
-            lps[i++]=0;
+            else{
+            lps[i]=0;
+            i++;
+            }
         }
     }
     return s.substr(0,lps[n-1]);
