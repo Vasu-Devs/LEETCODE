4ed997f7f7b3eef6c5a6bdbf3dfaf18d330097dd
Vasudev Siddh <143492267+Vasudev-Siddh@users.noreply.github.com>
2025-07-01 11:03:15 +0530

    Time: 10 ms (14.72%) | Memory: 15.6 MB (55.03%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
+
+
+    // ###############USING LPS#################
         string rev=s;
         reverse(rev.begin(),rev.end());
         string temp=s+"#"+rev;
         vector<int> lps(temp.length(),0);
         int len=0;
-    int i=1;
+        int i=1;
 
     while(i<temp.size()){
         if(temp[i]==temp[len]){
@@ -28,6 +31,15 @@ public:
     reverse(suf.begin(),suf.end());
     s=suf+s;
     return s;
-        
+    // int j=0,n=s.size();
+
+    // for(int i=n-1;i>=0;i--){
+    //     if(s[i]==s[j])
+    //         j++;
+    // }
+    // string suf=s.substr(j);
+    // reverse(suf.begin(),suf.end());
+    // s=suf+s;
+    // return s;
     }
 };
