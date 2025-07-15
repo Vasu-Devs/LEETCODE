59a451a57f992012c7b5a38926bfbf2ef3d769ff
Vasudev Siddh <143492267+Vasudev-Siddh@users.noreply.github.com>
2025-07-15 15:32:19 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (28.87%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -12,26 +12,32 @@
 class Solution {
 public:
     int rangeSumBST(TreeNode* root, int low, int high) {
-        vector<int> sot;
-        inorder(root,sot);
-        int i;
-        long long s=0;
-        for(i=0;i<sot.size();i++){
-            if(sot[i]>=low)
-                break;
-        }
-        while(i<sot.size() && sot[i]<=high){
-            s+=sot[i];
-            i++;
-        }
-        return s;
-        
-    }
-    void inorder(TreeNode* root , vector<int>& sot){
-        if (!root) return;
+        if(!root) return 0;
 
-        inorder(root->left,sot);
-        sot.push_back(root->val);
-        inorder(root->right,sot);
+        if(root->val<low)   return rangeSumBST(root->right,low,high);
+        if(root->val>high) return rangeSumBST(root->left, low , high);
+
+        return root->val + rangeSumBST(root->left, low , high) + rangeSumBST(root->right, low , high);
+    //     vector<int> sot;
+    //     inorder(root,sot);
+    //     int i;
+    //     long long s=0;
+    //     for(i=0;i<sot.size();i++){
+    //         if(sot[i]>=low)
+    //             break;
+    //     }
+    //     while(i<sot.size() && sot[i]<=high){
+    //         s+=sot[i];
+    //         i++;
+    //     }
+    //     return s;
+        
+    // }
+    // void inorder(TreeNode* root , vector<int>& sot){
+    //     if (!root) return;
+
+    //     inorder(root->left,sot);
+    //     sot.push_back(root->val);
+    //     inorder(root->right,sot);
     }
 };
